6081a0ca3b6275cd4808d2481805a3831ad7ffc9
Ishi <168891540+IshiPathak@users.noreply.github.com>
2026-07-25 18:15:00 +0530

    Day 8

--- 02_Arrays_Part_II/88_Merge_Sorted_Array.cpp
+++ 02_Arrays_Part_II/88_Merge_Sorted_Array.cpp
@@ -1,31 +1,39 @@
 class Solution {
+private:
+    void swapIfGreater(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2){
+        if(nums1[ind1] > nums2[ind2]){
+            swap(nums1[ind1],nums2[ind2]);
+        }
+    }
 public:
-    // Merges nums2 into nums1 in-place in sorted order.
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // i points to last valid element in nums1
-        int i = m - 1;
 
-        // j points to last element in nums2
-        int j = n - 1;
-
-        // k is the last index of nums1 (including 0 placeholders)
-        int k = m + n - 1;
-
-        // Fill nums1 from the end by comparing nums1[i] and nums2[j]
-        while (i >= 0 && j >= 0) {
-            // Place larger of the two at nums1[k]
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
+    void merge(vector<int>& nums1, int n, vector<int>& nums2, int m) {
+        int len = (n+m);
+        int gap = (len/2)+(len%2);
+        while(gap>0){
+            int left = 0;
+            int right = left+gap;
+            while(right<len){
+                //arr1 and arr2
+                if(left<n && right>=n){
+                    swapIfGreater(nums1,nums2,left,right-n);
+                }
+                //arr2 and arr2
+                else if(left>=n){
+                    swapIfGreater(nums2,nums2,left-n,right-n);
+                }
+                //arr1 and arr1
+                else{
+                    swapIfGreater(nums1,nums1,left,right);
+                }
+                left++,right++;
             }
+            if(gap==1) break;
+            gap = (gap/2)+(gap%2);
         }
-
-        // If nums2 has remaining elements, copy them
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+         // Copy nums2 into the vacant positions of nums1
+        for (int i = 0; i < m; i++) {
+            nums1[n + i] = nums2[i];
         }
-
-        // No need to copy remaining nums1 elements, as they are already in place
     }
 };
