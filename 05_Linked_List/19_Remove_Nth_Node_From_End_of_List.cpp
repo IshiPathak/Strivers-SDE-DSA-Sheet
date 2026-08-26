57a5924d8b533c88195faa3937ef54417c627984
Ishi <168891540+IshiPathak@users.noreply.github.com>
2026-08-26 23:28:08 +0530

    Day 28

--- 05_Linked_List/19_Remove_Nth_Node_From_End_of_List.cpp
+++ 05_Linked_List/19_Remove_Nth_Node_From_End_of_List.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    // Function to print the linked list
     void printLL(Node* head) {
         while (head != NULL) {
             cout << head->data << " ";
             head = head->next;
         }
     }
-
-    // Function to delete the Nth node from the end
     Node* deleteNthNodeFromEnd(Node* head, int N) {
         // If list is empty, return NULL
         if (head == NULL) {
@@ -18,13 +15,11 @@ public:
         int cnt = 0;
         Node* temp = head;
 
-        // Count the number of nodes in the linked list
         while (temp != NULL) {
             cnt++;
             temp = temp->next;
         }
 
-        // If N equals the total number of nodes, delete the head
         if (cnt == N) {
             Node* newHead = head->next;
             // free memory
@@ -32,11 +27,10 @@ public:
             return newHead;
         }
 
-        // Calculate the position from start to delete
         int res = cnt - N;
         temp = head;
 
-        // Traverse to the node just before the one to delete
+
         while (temp != NULL) {
             res--;
             if (res == 0) {
@@ -45,10 +39,9 @@ public:
             temp = temp->next;
         }
 
-        // Delete the target node
         Node* delNode = temp->next;
         temp->next = temp->next->next;
-        delete delNode; // free memory
+        delete delNode; 
 
         return head;
     }
